dd25e7ade5d19400d154dcb94ffbd33171792852
a20028891 <younes.AIT-KETTOUT@etu.univ-amu.fr>
2022-11-12 19:46:48 +0100

    h'

--- functions/api.js
+++ functions/api.js
@@ -28024,11 +28024,11 @@ const app = express();
 const router = express.Router();
 app.engine('html', mustache());
 app.set('view engine', 'html');
-app.set('views', './src/views');
+app.set('views', './dist/views');
 router.get("/", (req, res) => {
   res.render('index');
 });
-app.use('/images', express.static('./src/images'));
+app.use('/images', express.static('./dist/images'));
 app.use(`/.netlify/functions/api`, router);
 module.exports = app;
 module.exports.handler = serverless(app);
